21a2da3b65a0a1d434bc06819605afb127b19c17
xirrow <zachary.public@gmail.com>
2020-08-24 15:07:26 -0700

    Modified CSS, index.html, and JS. Added README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# Flashcard Site
+
+#### Calculator app - JavaScript
+
+#### By Zack Worrix
+
+## Showcasing show() and hide() effects with jQuery.
+
+# WARNING: Script if written to skill level. Not the most efficient practice. 
+
+## Setup/Installation
+
+* Clone this repository
+* Open files using your favorite web browser or IDE
+
+## Technologies used
+
+* HTML
+* CSS
+* JavaScript
+
+## Legal
+
+Copyright (c) 2020 **_Zack Worrix_**
+
+This work is licensed under the MIT license
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,155 @@
 jQuery(document).ready(function() {
-    jQuery("button#dark-mode").click(function() {
-        jQuery("body").removeClass();
-        jQuery("body").addClass("dark-mode");
-    });
-    jQuery("button#original-mode").click(function() {
-        jQuery("body").removeClass();
-        jQuery("body").addClass("original-mode");
+
+    jQuery(".clickable").click(function() {
+        jQuery("#showing").show('slow');
     });
 
-    var clickableBold = document.getElementsByClassName("clickable");
-    for (var i = 0; i < clickableBold.length; i++) {
-        clickableBold[i].style.fontWeight = "bold";
-    }
+    jQuery("#hidden").click(function() {
+        jQuery("#showing").hide('slow');
+    });
+
+    jQuery(".clickable2").click(function() {
+        jQuery("#showing2").show('slow');
+    });
+
+    jQuery("#hidden2").click(function() {
+        jQuery("#showing2").hide('slow');
+    });
+
+    jQuery(".clickable2").click(function() {
+        jQuery("#showing2").show('slow');
+    });
+
+    jQuery("#hidden3").click(function() {
+        jQuery("#showing3").hide('slow');
+    });
+
+    jQuery(".clickable3").click(function() {
+        jQuery("#showing3").show('slow');
+    });
+
+    jQuery("#hidden4").click(function() {
+        jQuery("#showing4").hide('slow');
+    });
+
+    jQuery(".clickable4").click(function() {
+        jQuery("#showing4").show('slow');
+    });
+
+    jQuery("#hidden4").click(function() {
+        jQuery("#showing4").hide('slow');
+    });
+
+    jQuery(".clickable5").click(function() {
+        jQuery("#showing5").show('slow');
+    });
+
+    jQuery("#hidden5").click(function() {
+        jQuery("#showing5").hide('slow');
+    });
+
+    jQuery(".clickable6").click(function() {
+        jQuery("#showing6").show('slow');
+    });
+
+    jQuery("#hidden6").click(function() {
+        jQuery("#showing6").hide('slow');
+    });
+
+    jQuery(".clickable7").click(function() {
+        jQuery("#showing7").show('slow');
+    });
+
+    jQuery("#hidden7").click(function() {
+        jQuery("#showing7").hide('slow');
+    });
+
+    jQuery(".clickable8").click(function() {
+        jQuery("#showing8").show('slow');
+    });
+
+    jQuery("#hidden8").click(function() {
+        jQuery("#showing8").hide('slow');
+    });
+
+    jQuery(".clickable9").click(function() {
+        jQuery("#showing9").show('slow');
+    });
+
+    jQuery("#hidden9").click(function() {
+        jQuery("#showing9").hide('slow');
+    });
+
+    jQuery(".clickable10").click(function() {
+        jQuery("#showing10").show('slow');
+    });
+
+    jQuery("#hidden10").click(function() {
+        jQuery("#showing10").hide('slow');
+    });
+
+    jQuery(".clickable11").click(function() {
+        jQuery("#showing11").show('slow');
+    });
+
+    jQuery("#hidden11").click(function() {
+        jQuery("#showing11").hide('slow');
+    });
+
+    jQuery(".clickable12").click(function() {
+        jQuery("#showing12").show('slow');
+    });
+
+    jQuery("#hidden12").click(function() {
+        jQuery("#showing12").hide('slow');
+    });
+
+    jQuery(".clickable13").click(function() {
+        jQuery("#showing13").show('slow');
+    });
+
+    jQuery("#hidden14").click(function() {
+        jQuery("#showing14").hide('slow');
+    });
+
+    jQuery(".clickable15").click(function() {
+        jQuery("#showing15").show('slow');
+    });
+
+    jQuery("#hidden15").click(function() {
+        jQuery("#showing16").hide('slow');
+    });
+
+    jQuery(".clickable16").click(function() {
+        jQuery("#showing16").show('slow');
+    });
+
+    jQuery("#hidden16").click(function() {
+        jQuery("#showing16").hide('slow');
+    });
+
+    jQuery(".clickable17").click(function() {
+        jQuery("#showing17").show('slow');
+    });
+
+    jQuery("#hidden17").click(function() {
+        jQuery("#showing17").hide('slow');
+    });
+
+    jQuery(".clickable18").click(function() {
+        jQuery("#showing18").show('slow');
+    });
+
+    jQuery("#hidden18").click(function() {
+        jQuery("#showing18").hide('slow');
+    });
+
+    jQuery(".clickable19").click(function() {
+        jQuery("#showing19").show('slow');
+    });
+
+    jQuery("#hidden19").click(function() {
+        jQuery("#showing19").hide('slow');
+    });
+
 });
